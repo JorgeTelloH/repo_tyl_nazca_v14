fd6bd8a96b8167c57a7a0b08198ad869cfc022c3
oswaldolosa <45778295+oswaldolosa@users.noreply.github.com>
2022-07-15 14:15:57 -0500

    mejoras y permisos nazca

--- cabalcon_nazca/models/booking.py
+++ cabalcon_nazca/models/booking.py
@@ -4,6 +4,7 @@ from odoo import models, fields, api
 
 class Address(models.Model):
     _name = 'tms.booking'
+    _inherit = 'image.mixin'
 
     name = fields.Char(
         'Booking', required=True)
